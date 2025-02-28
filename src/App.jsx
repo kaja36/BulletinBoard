03b50d7c3b0619c298c8f5afede4708e6fa91811
kaja36 <2412110185a@kindai.ac.jp>
2025-02-28 09:53:32 +0900

    掲示板メニュー作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
-import { useState,useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  
   const [threads, setThreads] = useState();
 
   useEffect(() => {
-  fetch("https://railway.bulletinboard.techtrain.dev/threads")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error: ${response.status}`);
-      }
-      return response.json();
-    }).then((data) => {
-      const threads = data.map((thread) => {
-        return <p class ="threadTitle">{thread.title}</p>
+    fetch("https://railway.bulletinboard.techtrain.dev/threads")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`);
+        }
+        return response.json();
       })
-      setThreads(threads);
-    });
-  },[]);
-
+      .then((data) => {
+        const threads = data.map((thread) => {
+          return <button>{thread.title}</button>;
+        });
+        setThreads(threads);
+      });
+  }, []);
   return (
     <>
-    <header>掲示板</header>
-      <h2>スレッド一覧</h2>
-      <hr></hr>
-      {threads}
+      <header>
+        <p class="header_title">掲示板</p>
+        <a class="header_button">
+          <u>スレッドを立てる</u>
+        </a>
+      </header>
+      <div class="thread_list">
+        <h2>新着スレッド</h2>
+        {threads}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
