cbfc28bc82866df1ca758912d4ef06392411c206
kaja36 <2412110185a@kindai.ac.jp>
2025-03-03 20:39:15 +0900

    スレッド一覧画面作成

--- src/threads/posts.jsx
+++ src/threads/posts.jsx
@@ -1,6 +1,8 @@
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 import Header from "../header";
+import "./posts.css";
 
 function Posts() {
   const [posts, setPosts] = useState();
@@ -11,27 +13,44 @@ function Posts() {
       "https://railway.bulletinboard.techtrain.dev/threads/" +
         thread_id +
         "/posts"
-    ).then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error: ${response.status}`);
-      }
-      return response.json();
-    }).then((data) => {
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
         const posts = data.map((post) => {
           return (
             <div key={post.id}>
               <p>{post.post}</p>
             </div>
           );
-        }
-      );
-      setPosts(posts);
-    });
+        });
+        setPosts(posts);
+      });
   }, []);
+
   return (
     <div>
       <Header />
-      {posts}
+      <div className="postPage">
+        <ul>
+          <li>
+            <h2>スレッドタイトル</h2>
+          </li>
+          <li>
+            <div className="postsList">{posts}あ</div>
+            <form className="postForm">
+              <label>
+                <textarea name="post" cols="20" rows="5" placeholder="投稿しよう！" />
+              </label>
+              <button type="submit">投稿</button>
+            </form>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
